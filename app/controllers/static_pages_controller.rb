fe337c534af17408ebd872417be6d074ef5488db
Arindrajit Kundu <arindrajit.kundu@gmail.com>
2018-09-25 16:32:22 +0530

    Cmplete authentication system

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+  	redirect_to articles_path if logged_in?
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+	root 'static_pages#home'
 	resources :articles
+
 	get 'signup', to: 'users#new'
 	resources :users, except: [:new]
-  root 'static_pages#home'
+
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
+
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
